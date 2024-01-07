3e0f2367e34ec949922608583cd1885822c996cd
Abu Huraira <abuhuraira.dt@gmail.com>
2024-01-07 17:06:41 +0300

    Refactor

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+
+    <h1>The home page of chess</h1>
   </main>
 </template>
