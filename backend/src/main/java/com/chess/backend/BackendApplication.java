3c40e763f6a639696c83a306379257cc7ea9813b
Abu Huraira <abuhuraira.dt@gmail.com>
2024-01-07 09:47:51 +0300

    Configure server

--- backend/src/main/java/com/chess/backend/BackendApplication.java
+++ backend/src/main/java/com/chess/backend/BackendApplication.java
@@ -3,8 +3,17 @@ package com.chess.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @SpringBootApplication
+@RestController
 public class BackendApplication {
+
+	@GetMapping("/")
+	public String index() {
+		return "Greetings from the chess server";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
